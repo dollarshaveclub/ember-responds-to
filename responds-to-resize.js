9086447fdd3697a14d382ca5c7762364fa0e06ac
Brian Gonzalez <me@briangonzalez.org>
2014-06-25 16:55:49 -0700

    fixed resizing ApplicationView to be interval-based, not based on resize..

--- responds-to-resize.js
+++ responds-to-resize.js
@@ -7,31 +7,24 @@
 require('../mixins/responds-to-resize');
 
 var RESIZE_EVENTS = 'resize orientationchange';
-var onResize;
 
 App.RespondsToResize = Ember.Mixin.create({
 
-  init: function () {
-    Ember.assert('RespondsToResize must be mixed in to a View', this instanceof Ember.View);
-    this._super();
-    var self = this;
-    onResize = function (e) {
-      window.requestAnimationFrame( self.resize.bind(self) );
-    };
-  },
-
   didInsertElement: function () {
-    this._super();
-    this.resize();
-    $(window).on(RESIZE_EVENTS, this, onResize.bind(this));
+    Ember.assert('RespondsToResize must be mixed in to a View', this instanceof Ember.View);
+    $(window).on( RESIZE_EVENTS, this.debouncedResize.bind(this) );
   },
 
   willDestroyElement: function () {
-    $(window).off(RESIZE_EVENTS, this, onResize.bind(this));
+    $(window).off( RESIZE_EVENTS, this.debouncedResize.bind(this) );
     this._super();
   },
 
-  resize: function () {
+  debouncedResize: function(e) {
+    window.requestAnimationFrame( this.resize.bind(this, e) );
+  },
+
+  resize: function (e) {
     console.warn('[RespondsToResize] Please define a resize function in the view using this mixin.');
   }
 });
