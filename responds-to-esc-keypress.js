c4fd56fd8cd3d59439a22475760dc35c21a9b246
Patrick Fisher <patrick@pwfisher.com>
2014-08-01 16:14:48 -0700

    Rewrite responds-to-esc-keypress to call listeners in LIFO order and allow listeners to stop propagation; all listeners were responding at all times

--- responds-to-esc-keypress.js
+++ responds-to-esc-keypress.js
@@ -7,28 +7,35 @@
 var ESC_ACTION_NAME = 'escKeypress';
 var onKeypress;
 var ESC_CODE = 27;
+var listeners = []; // Used to call listeners in LIFO order
+
+$(window).on('keydown', this, function (e) {
+  if (e.which !== ESC_CODE) return;
+  if (['SELECT', 'INPUT'].indexOf(e.target.tagName) > -1) return;
+  listeners.some(function (listener) {
+    return listener.escKeypress();
+  });
+});
 
 App.RespondsToEscKeypress = Ember.Mixin.create({
 
   init: function () {
-    Ember.assert('RespondsToEscKeypress must be mixed in to a View', this instanceof Ember.View);
     this._super();
-    var self = this;
-
-    onKeypress = function (e) {
-      if (e.which !== ESC_CODE) return;
-      if (['SELECT', 'INPUT'].indexOf(e.target.tagName) > -1) return;
-      self.send(ESC_ACTION_NAME);
-    };
+    Ember.assert('RespondsToEscKeypress must be mixed in to a View', this instanceof Ember.View);
   },
+
+  // @return {boolean} stopPropagation
+  escKeypress: function () { },
 
   didInsertElement: function () {
     this._super();
-    $(window).on('keydown', this, onKeypress);
+    listeners.unshift(this);
   },
 
-  willDestroyElement: function () {
-    $(window).off('keydown', this, onKeypress);
+  willClearRender: function () {
     this._super();
+    listeners = listeners.filter(function (listener) {
+      return listener !== this;
+    }, this);
   }
 });
