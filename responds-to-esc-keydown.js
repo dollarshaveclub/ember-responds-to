8a1e85d05421e4b30e804d1b72fcc888746eb215
Patrick Fisher <patrick@pwfisher.com>
2014-10-10 16:34:44 -0700

    Use keydown instead of keypress for reliability

--- responds-to-esc-keydown.js
+++ responds-to-esc-keydown.js
@@ -1,5 +1,5 @@
 
-// Calls this.escKeypress() on ESC keydown
+// Calls this.escKeydown() on ESC keydown
 // --------------------------------------
 // Remember to call this._super() if you override didInsertElement or willClearRender.
 //
@@ -7,25 +7,25 @@
 var ESC_CODE = 27;
 var listeners = [];
 
-// Calls handler on each View which RespondsToEscKeypress in
+// Calls handler on each View which RespondsToEscKeydown in
 // LIFO order, unless target element is a SELECT or INPUT.
 $(window).on('keydown', this, function (e) {
   if (e.which !== ESC_CODE) return; // we only handle ESC
   if (['SELECT', 'INPUT'].indexOf(e.target.tagName) > -1) return;
   listeners.some(function (listener) {
-    return listener.escKeypress();
+    return listener.escKeydown();
   });
 });
 
-App.RespondsToEscKeypress = Ember.Mixin.create({
+App.RespondsToEscKeydown = Ember.Mixin.create({
 
   init: function () {
     this._super();
-    Ember.assert('RespondsToEscKeypress must be mixed in to a View', this instanceof Ember.View);
+    Ember.assert('RespondsToEscKeydown must be mixed in to a View', this instanceof Ember.View);
   },
 
   // @return {boolean} stopPropagation
-  escKeypress: function () { },
+  escKeydown: function () { },
 
   didInsertElement: function () {
     this._super();
